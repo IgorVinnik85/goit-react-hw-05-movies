27f3faa37f8de5c039434ba3ffb1a9fc3540d6c1
Igor <115535399+IgorVinnik85@users.noreply.github.com>
2023-05-25 16:48:27 +0300

    update_reviews

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -4,17 +4,23 @@ import { useParams } from 'react-router-dom';
 
 const Reviews = () => {
   const [detailsData, setDetailsData] = useState(null);
+ const [activeReviewId, setActiveReviewId] = useState(null);
   const params = useParams();
   const detailsId = Number(params.movieName);
 
   useEffect(() => {
     reviews(detailsId)
       .then(({ data }) => {
-        console.log(data.results);
+        // console.log(data.results);
         setDetailsData(data.results);
       })
       .catch(err => console.log(err));
   }, [detailsId]);
+
+  const showText = id => {
+    console.log(id)
+    setActiveReviewId(id === activeReviewId ? null : id);
+  };
 
   return (
     <>
@@ -23,9 +29,9 @@ const Reviews = () => {
           <ul>
             {detailsData.map(({ author, content, id }) => {
               return (
-                <li key={id}>
+                <li onClick={() => showText(id)} key={id}>
                   <h2>{author}</h2>
-                  <p>{content}</p>
+                  {activeReviewId === id && <p>{content}</p>}
                 </li>
               );
             })}
