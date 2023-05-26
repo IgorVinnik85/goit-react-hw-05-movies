6de42955f4406565d442ca19d8144124f0d52f7e
Igor <115535399+IgorVinnik85@users.noreply.github.com>
2023-05-26 12:38:53 +0300

    update_css

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { Ul, LinkList } from './SearchList.styled';
 
 const SearchList = ({ list }) => {
--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {
   useParams,
-  Link,
   Outlet,
   useLocation,
   useNavigate,
